611a2853d4f87f54de8c9634b03165e9c541acee
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2022-01-18 15:20:19 -0500

    got layout of Header started, also started styles

---
+++ my-app/src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.25)
+  }
+}));
